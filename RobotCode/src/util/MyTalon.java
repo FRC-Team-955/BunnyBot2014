cadaf0266815057032433e2331ddc0a0672a9a67
Programming <Programming@ROBOTICS-PC.CSD509J.Net>
2014-11-05 19:00:02 -0800

    Added MyTalon

--- RobotCode/src/util/MyTalon.java
+++ RobotCode/src/util/MyTalon.java
@@ -1,10 +1,33 @@
 package util;
+import edu.wpi.first.wpilibj.Talon;
+import java.lang.Math;
 
 /**
  *
  * @author Programming
  */
-public class MyTalon 
+
+public class MyTalon extends Talon 
 {
-    
+
+    public MyTalon(int channel) 
+    {
+        super(channel);
+    }
+
+    public void ramp(double required) 
+    {
+        if (Math.abs(super.get() - required) > Config.Drive.maxRampRate) 
+        {
+
+            if (required > get()) 
+                set(super.get() + Config.Drive.maxRampRate);
+            
+            else 
+                set(super.get() - Config.Drive.maxRampRate);
+        } 
+        
+        else 
+            set(required);
+    }
 }
